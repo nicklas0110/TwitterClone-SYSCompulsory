d3eb784f6e81eb822b003c061ede31f83e5de9e1
mols14 <71692370+mols14@users.noreply.github.com>
2024-04-22 14:25:22 +0200

    UserService mostly done
    most classes if not all

---
+++ UserService/Core/Services/DTOs/GetUserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace UserService.Core.Services.DTOs;
+
+public class GetUserDTO
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string Surname { get; set; }
+    public DateTime CreationDate { get; set; }
+}
